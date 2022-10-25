6b0283f1079d8c760c1ba2c2315e55742be18e73
SRINIVASAN N R <68799172+srinivasannr@users.noreply.github.com>
2022-10-25 08:54:43 +0530

    Add files via upload

---
+++ MazeSolver/src/project/Dfs.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author SRINIVASAN N R
+ */
+public class Dfs {
+    public static boolean searchPath(int [][] maze,int x,int y ,List<Integer> path){
+       
+        
+        if(maze[x][y]==9){
+            path.add(x);
+            path.add(y);
+            return true;
+        }
+        
+        if(maze[x][y]==0){
+            maze[x][y]=2;
+            int [] dx={1,0,-1,0};
+            int [] dy={0,-1,0,1};
+            
+            for(int d=0;d<dx.length;d++){
+                int newx=x+dx[d];
+                int newy=y+dy[d];
+                if(newx>=0 && newx<maze.length && newy>=0 && newy<maze[0].length && searchPath(maze,newx,newy,path)){
+                    path.add(x);
+                    path.add(y);
+                
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+    public static void main(String [] args){
+        int x=0;
+        int y=0;
+        int a[][]={{0,0,0},
+            {0,1,1},
+            {9,1,0}};
+        List<Integer> list=new ArrayList<>();
+        System.out.print(searchPath(a,x,y,list));
+        System.out.print(list);
+        
+        
+    }
+    
+}
